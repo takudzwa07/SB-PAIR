bd14ea62edc9c3ca4fd77bfcd1bba74cd157a37c
takudzwa07 <mrfrankofc17@gmail.com>
2025-04-19 20:08:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.listen(PORT, () => {
     console.log(`
 Deployment Successful!
 
- Gifted-Session-Server Running on http://localhost:` + PORT)
+ Subzero-Server Running on http://localhost:` + PORT)
 })
 
 module.exports = app
